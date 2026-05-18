c9cd0b4b465675d9d023942e0a53deee0a650e29
Arseniy0203 <fedor0varseniy@yandex.ru>
2026-05-19 02:29:46 +0300

    Реализовал posting

--- posting.c
+++ posting.c
@@ -1,12 +1,33 @@
 #include <string.h>
 #include "posting.h"
 
-Vector* createPostingList(void) {
+Vector* createPostingList(void) 
+{
+    // создаем вектор для хранения записей
     return createVector(sizeof(PostingEntry));
 }
 
-void appendPosting(Vector* list, int doc_id, const char* title) {
-    if (!list) return;
+void appendPosting(Vector* list, int doc_id, const char* title) 
+{
+    if (list == NULL) 
+    {
+        return;
+    }
+
+
+    // отсекаем дубликаты
+    if (list->size > 0) 
+    {
+        PostingEntry* last_entry = (PostingEntry*)getVectorItem(list, list->size - 1);
+        
+
+        if (last_entry->doc_id == doc_id) 
+        {
+            return; 
+        }
+    }
+
+    // добавление записи в вектор
     PostingEntry entry;
     entry.doc_id = doc_id;
     strncpy(entry.title, title, MAX_TITLE_LEN - 1);
@@ -15,9 +36,20 @@ void appendPosting(Vector* list, int doc_id, const char* title) {
 }
 
 Vector* clonePostingList(const Vector* src) {
-    if (!src) return NULL;
+    if (src == NULL) 
+    {
+        return NULL;
+    }
+
+
     Vector* clone = createPostingList();
+
+
     for (size_t i = 0; i < src->size; i++)
+    {
         appendVectorItem(clone, getVectorItem((Vector*)src, i));
+    }
+
+
     return clone;
 }
